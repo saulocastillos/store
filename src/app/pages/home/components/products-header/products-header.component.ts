bb8595a3fd0721e571ee6dd1fa492bff3c40ac52
Saulo <saulo.castilhos@objective.com.br>
2022-11-27 20:50:05 -0300

    feat: add multiple views for list

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core"
+import { Component, EventEmitter, Output } from "@angular/core"
 
 @Component({
   selector: "app-products-header",
   templateUrl: "./products-header.component.html",
 })
 export class ProductsHeaderComponent {
+  @Output() columnsCountChange = new EventEmitter<number>()
   sort = "desc"
   itemsShowCount = 12
 
@@ -14,5 +15,9 @@ export class ProductsHeaderComponent {
 
   onItemsUpdated(count: number): void {
     this.itemsShowCount = count
+  }
+
+  onColumnsUpdate(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum)
   }
 }
